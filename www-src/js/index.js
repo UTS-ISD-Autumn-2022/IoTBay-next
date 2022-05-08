a9f3ae72aa4b8c3e3a24619dba1aaf5f7f150c9a
Euan Mendoza <bebopbamf@effectfree.dev>
2022-05-08 21:15:31 +1000

    main form fix

--- www-src/js/index.js
+++ www-src/js/index.js
@@ -1,6 +1,2 @@
 import '@popperjs/core';
-import * as bootstrap from 'bootstrap';
-
-const modal = new bootstrap.Modal(document.getElementById('welcomeModal'));
-modal.show();
-
+import * as bs from 'bootstrap';
