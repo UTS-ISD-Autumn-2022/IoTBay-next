445a2d147c101ff2c763900ee66c0b134e0410dd
Euan Mendoza <bebopbamf@effectfree.dev>
2022-05-16 12:11:06 +1000

    fixing database

--- sql/01-auth.sql
+++ sql/01-auth.sql
@@ -43,3 +43,11 @@ CREATE TABLE payment_details (
     card_expiry_day varchar (2) NOT NULL,
     card_expiry_year varchar (4) NOT NULL
 );
+
+INSERT INTO users (username, password, enabled)
+    VALUES
+    ('admin', '$2a$12$c3dUM4Wws7E9umjkWgDiVuxr/SQ657sBJW6TEnGqBdlgAEGBuz.Iq', true);
+
+INSERT INTO authorities (username, authority)
+    VALUES
+    ('admin', 'ADMIN');
