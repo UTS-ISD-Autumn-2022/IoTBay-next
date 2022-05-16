dd3762ec8a62fd5d6917be47e2447d5fddfd0872
Euan Mendoza <bebopbamf@effectfree.dev>
2022-05-16 12:31:53 +1000

    modified types to be numeric

--- sql/02-payment.sql
+++ sql/02-payment.sql
@@ -6,10 +6,10 @@ CREATE TABLE payment_Option (
 Create TABLE CARD_PAYEMNT (
     id uuid PRIMARY KEY,
     card_name varchar (255) NOT NULL,
-    card_number varchar (16) NOT NULL,
-    card_cvc varchar (3) NOT NULL,
-    card_expiry_month varchar (2) NOT NULL,
-    card_expiry_year varchar (4) NOT NULL,
+    card_number numeric (16) NOT NULL,
+    card_cvc numeric (3) NOT NULL,
+    card_expiry_month numeric (2) NOT NULL,
+    card_expiry_year numeric (4) NOT NULL,
 
     FOREIGN KEY (id) REFERENCES payment_option (id)
     );
@@ -17,8 +17,8 @@ Create TABLE CARD_PAYEMNT (
 CREATE TABLE BANK_PAYMENT(
     id uuid PRIMARY KEY,
     BANK_NAME varchar (255) NOT NULL,
-    BANK_ACCOUNT_NUMBER varchar (9) NOT NULL,
-    BSB_NUMBER varchar (6) NOT NULL,
+    BANK_ACCOUNT_NUMBER numeric (9) NOT NULL,
+    BSB_NUMBER numeric (6) NOT NULL,
 
     FOREIGN KEY (id) REFERENCES payment_option (id)
 );
