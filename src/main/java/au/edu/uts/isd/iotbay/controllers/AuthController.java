d34caf7ac1dd73be6a2832013ab37ed900fc9912
Euan Mendoza <bebopbamf@effectfree.dev>
2022-05-18 10:24:33 +1000

    unit tests

--- src/main/java/au/edu/uts/isd/iotbay/controllers/AuthController.java
+++ src/main/java/au/edu/uts/isd/iotbay/controllers/AuthController.java
@@ -1,7 +1,6 @@
 package au.edu.uts.isd.iotbay.controllers;
 
 import au.edu.uts.isd.iotbay.models.dao.UserManager;
-import au.edu.uts.isd.iotbay.models.data.Customer;
 import au.edu.uts.isd.iotbay.models.forms.RegisterForm;
 
 import javax.validation.Valid;
@@ -11,12 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
@@ -39,7 +35,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String registerPost(@Valid RegisterForm registerForm, BindingResult result, Model model) {
+    public String registerPost(@Valid RegisterForm registerForm, BindingResult result) {
         if (result.hasErrors()) {
             for (val e : result.getAllErrors())
                 logger.error(e.getDefaultMessage());
--- src/main/java/au/edu/uts/isd/iotbay/models/forms/RegisterForm.java
+++ src/main/java/au/edu/uts/isd/iotbay/models/forms/RegisterForm.java
@@ -3,30 +3,33 @@ package au.edu.uts.isd.iotbay.models.forms;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 public class RegisterForm {
-    @Size(max = 255)
+    @Size(max = 50)
+    @NotNull
     private String username;
     
     @Size(min = 8)
+    @NotNull
     private String password;
 
     @Size(min = 8)
+    @NotNull
     private String passwordVerification;
 
     @Email
     @Size(max = 255)
+    @NotNull
     private String email;
 
     @Size(max = 63)
+    @NotNull
     private String firstName;
 
     @Size(max = 63)
+    @NotNull
     private String lastName;
 }
