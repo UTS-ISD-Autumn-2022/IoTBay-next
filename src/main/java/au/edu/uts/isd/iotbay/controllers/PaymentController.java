26de2885a0300f75bb5c406bb83990894746d8e6
Euan Mendoza <bebopbamf@effectfree.dev>
2022-05-18 15:14:54 +1000

    fixed controller

--- src/main/java/au/edu/uts/isd/iotbay/controllers/PaymentController.java
+++ src/main/java/au/edu/uts/isd/iotbay/controllers/PaymentController.java
@@ -1,6 +1,8 @@
 
 package au.edu.uts.isd.iotbay.controllers;
 
+import au.edu.uts.isd.iotbay.models.dao.BankPaymentManager;
+import au.edu.uts.isd.iotbay.models.dao.CardPaymentManager;
 import au.edu.uts.isd.iotbay.models.dao.InventoryManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,13 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/payment")
+@RequestMapping("/profile/payment")
 public class PaymentController {
     @Autowired
-    InventoryManager inventoryManager;
+    BankPaymentManager bankPaymentManager;
 
-    @GetMapping("/")
+    @Autowired
+    CardPaymentManager cardPaymentManager;
+
+    @GetMapping("")
     String indexGet() {
-        return "payment";
+        return "payment/index";
     }
 }
