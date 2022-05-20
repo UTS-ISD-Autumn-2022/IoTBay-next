eba3e759de9def36e6c12b5c9fa8aa53fb3581a2
Euan Mendoza <bebopbamf@effectfree.dev>
2022-05-20 21:14:36 +1000

    unit tests complete

--- src/test/java/au/edu/uts/isd/iotbay/ProfileTests.java
+++ src/test/java/au/edu/uts/isd/iotbay/ProfileTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,28 +22,21 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-/*
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProfileTests {
     final private Logger log = LoggerFactory.getLogger(ProfileTests.class);
-
-    @MockBean
-    private DataSource dataSource;
-
-    @MockBean
-    private UserManager userManager;
 
     @Autowired
     private MockMvc mvc;
 
-    @BeforeEach
-    void setup() {
-        mvc = standaloneSetup(new ProfileController()).build();
-    }
+    @MockBean
+    private UserManager userManager;
 
     /**
      * <h3>As a customer, I want to view my account details so that I can ensure they are valid</h3>
@@ -55,7 +49,6 @@ public class ProfileTests {
      *
      * @result http: 200 OK, user details
      */
-/*
     @Test
     @DisplayName("UA-4: Read Customer Profile Test")
     void testViewCustomer() throws Exception {
@@ -65,13 +58,17 @@ public class ProfileTests {
         user.setFirstName("First");
         user.setLastName("Last");
 
-        final Customer cust = new Customer(UUID.randomUUID(), user);
+        final Customer customer = new Customer(UUID.randomUUID(), user);
 
-        given(userManager.fetchCustomerByUsername(user.getUsername())).willReturn(cust);
+        given(userManager.fetchCustomerByUsername(user.getUsername())).willReturn(customer);
 
-        mvc.perform(get("/profile")).andExpect(status().isOk());
+        mvc.perform(get("/profile").with(user("user_name").roles("CUSTOMER")))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("customer", customer))
+                .andExpect(view().name("profile/index"));
     }
-
+}
+/*
     /**
      * <h3>User Story:</h3>
      *
